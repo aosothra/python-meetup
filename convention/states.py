979727c439c2780de9881836cab23af31a231999
aosothra-remote <aosothra@github.com>
2022-08-03 17:39:45 +0300

    create states for attendee signup

--- convention/states.py
+++ convention/states.py
@@ -1,5 +1,6 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
+from convention.models import Attendee
 
 from convention.state_machine import State
 
@@ -19,17 +20,133 @@ class MenuState(State):
         )
 
     def handle_input(self, update: Update, context: CallbackContext):
+        chat_id = update.effective_chat.id
+
         if not update.callback_query:
             return None
 
         answer = update.callback_query.data
         update.callback_query.answer()
-        print(answer)
+
         if answer == "schedule":
             return SchedulePickEventState()
+        elif answer == "networking":
+            attendee, new = Attendee.objects.get_or_create(telegram_id=chat_id)
+            if new or attendee.is_anonymous():
+                return SignupNameState()
 
     def clean_up(self, update: Update, context: CallbackContext):
         self.message.edit_reply_markup()
+
+
+class SignupNameState(State):
+    def display_data(self, chat_id: int, update: Update, context: CallbackContext):
+        context.bot.send_message(
+            chat_id=chat_id,
+            text='Похоже что мы с Вами еще не знакомы. Скажите, пожалуйста, как Вас зовут ("Имя Фамилия"):',
+        )
+
+    def handle_input(self, update: Update, context: CallbackContext):
+        if not update.message:
+            return None
+
+        answer = update.message.text
+        firstname, lastname = answer.split(" ", 1)
+        context.user_data["firstname"] = firstname
+        context.user_data["lastname"] = lastname
+
+        return SignupCompanyState()
+
+
+class SignupCompanyState(State):
+    def display_data(self, chat_id: int, update: Update, context: CallbackContext):
+        firstname = context.user_data["firstname"]
+        lastname = context.user_data["lastname"]
+        context.bot.send_message(
+            chat_id=chat_id,
+            text=f"Приятно познакомиться, {firstname} {lastname}. Скажите, пожалуйста, где Вы работаете?",
+        )
+
+    def handle_input(self, update: Update, context: CallbackContext):
+        if not update.message:
+            return None
+
+        answer = update.message.text
+        context.user_data["company"] = answer
+        return SignupPositionState()
+
+
+class SignupPositionState(State):
+    def display_data(self, chat_id: int, update: Update, context: CallbackContext):
+        self.message = context.bot.send_message(
+            chat_id=chat_id,
+            text="Скажите, пожалуйста, какая у Вас должность в компании?",
+        )
+
+    def handle_input(self, update: Update, context: CallbackContext):
+        chat_id = update.effective_chat.id
+
+        if not update.message:
+            return None
+
+        answer = update.message.text
+        context.user_data["position"] = answer
+        return SignupConfirmState()
+
+
+class SignupConfirmState(State):
+    def display_data(self, chat_id: int, update: Update, context: CallbackContext):
+        firstname = context.user_data["firstname"]
+        lastname = context.user_data["lastname"]
+        company = context.user_data["company"]
+        position = context.user_data["position"]
+        message_text = (
+            "Пожалуйста проверьте анкету, убедитесь что все верно:"
+            f"\n{firstname} {lastname}\n{position}\n{company}"
+        )
+
+        menu_keyboard = [
+            [InlineKeyboardButton("Подтвердить", callback_data="confirm")],
+            [InlineKeyboardButton("Отменить", callback_data="cancel")],
+        ]
+
+        self.message = context.bot.send_message(
+            chat_id=chat_id,
+            text=message_text,
+            reply_markup=InlineKeyboardMarkup(menu_keyboard),
+        )
+
+    def handle_input(self, update: Update, context: CallbackContext):
+        chat_id = update.effective_chat.id
+
+        if not update.callback_query:
+            return None
+
+        answer = update.callback_query.data
+        if answer == "confirm":
+            attendee = Attendee.objects.get(telegram_id=chat_id)
+            attendee.firstname = context.user_data["firstname"]
+            attendee.lastname = context.user_data["lastname"]
+            attendee.company = context.user_data["company"]
+            attendee.position = context.user_data["position"]
+            attendee.save()
+
+            context.bot.send_message(
+                chat_id=chat_id,
+                text="Отлично! Теперь мы сможем познакомиться с другими участниками.",
+            )
+        else:
+            context.bot.send_message(
+                chat_id=chat_id, text="Ничего страшного, мы можем попробовать снова."
+            )
+        return MenuState()
+
+    def clean_up(self, update: Update, context: CallbackContext):
+        self.message.delete()
+        context.user_data.pop("firstname", None)
+        context.user_data.pop("lastname", None)
+        context.user_data.pop("company", None)
+        context.user_data.pop("position", None)
 
 
 class SchedulePickEventState(State):
