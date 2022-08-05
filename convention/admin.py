1cc4981661d9d9c14d9d8deb7c6499f07071f04b
Roman Volodin <roman.volodin@gmail.com>
2022-08-05 19:29:24 +0300

    Setup list display in Django admin

--- convention/admin.py
+++ convention/admin.py
@@ -5,24 +5,73 @@ from convention.models import Attendee, Block, Event, Flow, Presentation
 
 @admin.register(Attendee)
 class AttendeeAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "firstname",
+        "lastname",
+        "telegram_id",
+        "telegram_username",
+        "company",
+        "position",
+    )
+    readonly_fields = (
+        "telegram_id",
+        "telegram_username",
+    )
+    search_fields = (
+        "firstname",
+        "lastname",
+        "telegram_id",
+        "telegram_username",
+        "company",
+        "position",
+    )
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "title",
+        "starting_date",
+        "ending_date",
+    )
+    list_filter = (
+        "starting_date",
+        "ending_date",
+    )
+    search_fields = ("title",)
 
 
 @admin.register(Flow)
 class FlowAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "event",
+        "title",
+    )
+    search_fields = (
+        "event",
+        "title",
+    )
 
 
 @admin.register(Block)
 class BlockAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "flow",
+        "title",
+        "moderator",
+        "starts_at",
+        "ends_at",
+    )
+    search_fields = (
+        "title",
+        "moderator",
+    )
 
 
 @admin.register(Presentation)
 class PresentationAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "block",
+        "title",
+    )
+    search_fields = ("title",)
