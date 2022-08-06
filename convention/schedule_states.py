3af82ea4c29793a2ff034cba8c12ae3898e3806d
aosothra-remote <aosothra@github.com>
2022-08-06 12:21:51 +0300

    fix `menu_keyboard` assignment

--- convention/schedule_states.py
+++ convention/schedule_states.py
@@ -104,15 +104,11 @@ class ScheduleShowPresentationsState(State):
 
     def display_data(self, chat_id: int, update: Update, context: CallbackContext):
         menu_keyboard = [
-            # [InlineKeyboardButton(block.title, callback_data=f"{block.id}")]
-            # for block in self.flow.blocks.all()
-        ]
-        menu_keyboard.append(
             [
                 InlineKeyboardButton("Назад", callback_data="back"),
                 InlineKeyboardButton("Вернуться в меню", callback_data="menu"),
             ]
-        )
+        ]
         self.message = context.bot.send_message(
             chat_id=chat_id,
             text=render_to_string(
